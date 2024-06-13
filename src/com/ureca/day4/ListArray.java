c6a1672f32ebf0aa068b0e2930e1f9711df8e342
student <student@M4039>
2024-06-13 16:21:53 +0900

    hashSet

--- src/com/ureca/day4/ListArray.java
+++ src/com/ureca/day4/ListArray.java
@@ -1,5 +1,29 @@
 package com.ureca.day4;
 
-public class ListArray {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class ListArray {
+	public static void main(String[] args) {
+		int[] ia = new int[3];
+		ia[0] = 'A';
+		System.out.println(Arrays.toString(ia));
+		System.out.println();
+		
+		List<Integer>[] la = new List[3]; //new List<>[3] x 
+		
+		System.out.println(Arrays.toString(la));
+		
+		//la[0].add(123); 아래 for문을 하기 전에 이렇게 하면 널포인트에셉션 왜? la 배열안이 지금 null이야 
+		
+		for(int i = 0; i<la.length; i++) la[i] = new ArrayList<>(); // 각 원소에 객체 넣어주기
+		
+		la[0].add(123);
+		
+		System.out.println(la[0]);
+		System.out.println(Arrays.toString(la));
+		
+		
+	}
 }
