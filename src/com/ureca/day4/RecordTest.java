ca723e139055468cb779303b3ea4c8af055edc36
student <student@M4039>
2024-06-13 14:26:32 +0900

    record lombok

--- src/com/ureca/day4/RecordTest.java
+++ src/com/ureca/day4/RecordTest.java
@@ -1,11 +1,14 @@
 package com.ureca.day4;
 
-record Member(String id, String name , int age) {
-	
-}
+
 
 
 public class RecordTest {
+	
+	//다른 파일에 Member클래스 만들기 위해서 얘는 그냥 중첩 클래스로 만들어버리기 얘는 사실상 RecordTest.Member의 이름을 가짐
+	record Member(String id, String name , int age) {
+		
+	}
 
 	public static void main(String[] args) {
 		Member m1 = new Member("Winter","눈송이",25);
